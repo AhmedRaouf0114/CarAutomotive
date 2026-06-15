7289e143831032013456961a1fe2a051dced0548
Muhamed Husseny <muhamedhusseny288@gmail.com>
2026-06-15 19:29:19 +0300

    refactor responsecache

--- CarAutomotive.Infrastructure/Services/ResponseCacheService.cs
+++ CarAutomotive.Infrastructure/Services/ResponseCacheService.cs
@@ -1,4 +1,5 @@
 ﻿using StackExchange.Redis;
+
 namespace CarAutomotive.Infrastructure.Services
 {
     public class ResponseCacheService : IResponseCacheService
@@ -6,44 +7,44 @@ namespace CarAutomotive.Infrastructure.Services
         private readonly IConnectionMultiplexer _redis;
         private readonly IDatabase _database;
 
-        public ResponseCacheService(
-            IConnectionMultiplexer redis)
+        public ResponseCacheService(IConnectionMultiplexer redis)
         {
             _redis = redis;
             _database = redis.GetDatabase();
         }
 
-        public async Task  CacheResponseAsync(string cacheKey, object response, TimeSpan timeToLive)
+        public async Task CacheResponseAsync(string cacheKey, object response, TimeSpan timeToLive)
         {
             if (response is null) return;
+
             var serializeOptions = new JsonSerializerOptions
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase
             };
+
             var serializeResponse = JsonSerializer.Serialize(response, serializeOptions);
-            await _database.StringSetAsync(cacheKey,serializeResponse, timeToLive);
+            await _database.StringSetAsync(cacheKey, serializeResponse, timeToLive);
         }
 
         public async Task<string?> GetCachedResponseAsync(string cacheKey)
         {
-           var cachedResponse = await _database.StringGetAsync(cacheKey);
-           if(cachedResponse.IsNullOrEmpty) return null;
-           return cachedResponse;
+            var cachedResponse = await _database.StringGetAsync(cacheKey);
+
+            if (cachedResponse.IsNullOrEmpty) return null;
+
+            return cachedResponse;
         }
 
         public async Task RemoveCacheResponseAsync(string pattern)
         {
-            var server = _redis.GetServer(
-                _redis.GetEndPoints().First());
+            var server = _redis.GetServer(_redis.GetEndPoints().First());
 
-            var keys = server.Keys(pattern: $"{pattern}*").ToArray();
+            var keys = server.Keys(database: _database.Database, pattern: $"{pattern}*").ToArray();
+
             if (!keys.Any())
                 return;
-            foreach (var key in keys)
-            {
-                await _database.KeyDeleteAsync(key);
-            }
-        }
+
+            await _database.KeyDeleteAsync(keys);
         }
     }
 }
